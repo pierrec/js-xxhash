b61a44dad7fe1ac7ae1d7200223e553d2fc46772
Pierre Curto <pierre.curto@gmail.com>
2015-03-12 21:40:54 +0100

    Fix for issue #5: convert all strings to utf-8

--- test/XXH-test.js
+++ test/XXH-test.js
@@ -127,4 +127,46 @@ describe('XXH', function () {
 
 	})
 
+	describe('with utf-8 strings', function () {
+		var input = 'heiå'
+		var expected = 'DB5ABCCC' // Computed with xxHash C version
+
+		it('should return hash', function (done) {
+			var h = XXH( input, seed ).toString(16).toUpperCase()
+
+			assert.equal( h, expected )
+			done()
+		})
+
+		it('should return hash in many steps', function (done) {
+			var H = XXH( seed )
+			var h = H.update( input ).digest().toString(16).toUpperCase()
+
+			assert.equal( h, expected )
+			done()
+		})
+
+	})
+
+	describe('with utf-8 strings', function () {
+		var input = 'κόσμε'
+		var expected = 'D855F606' // Computed with xxHash C version
+
+		it('should return hash', function (done) {
+			var h = XXH( input, seed ).toString(16).toUpperCase()
+
+			assert.equal( h, expected )
+			done()
+		})
+
+		it('should return hash in many steps', function (done) {
+			var H = XXH( seed )
+			var h = H.update( input ).digest().toString(16).toUpperCase()
+
+			assert.equal( h, expected )
+			done()
+		})
+
+	})
+
 })
--- xxhash.js
+++ xxhash.js
@@ -61,42 +61,42 @@
 
 	var PRIME32_1plus2 = PRIME32_1.clone().add(PRIME32_2)
 
-    /**
-     * Convert string to proper UTF-8 array
-     * @param str Input string
-     * @returns {Uint8Array} UTF8 array is returned as uint16 array
-     */
-    function toUTF8Array(str) {
-        var utf8 = [];
-        for (var i=0; i < str.length; i++) {
-            var charcode = str.charCodeAt(i);
-            if (charcode < 0x80) utf8.push(charcode);
-            else if (charcode < 0x800) {
-                utf8.push(0xc0 | (charcode >> 6),
-                    0x80 | (charcode & 0x3f));
-            }
-            else if (charcode < 0xd800 || charcode >= 0xe000) {
-                utf8.push(0xe0 | (charcode >> 12),
-                    0x80 | ((charcode>>6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
-            }
-            // surrogate pair
-            else {
-                i++;
-                // UTF-16 encodes 0x10000-0x10FFFF by
-                // subtracting 0x10000 and splitting the
-                // 20 bits of 0x0-0xFFFFF into two halves
-                charcode = 0x10000 + (((charcode & 0x3ff)<<10)
-                | (str.charCodeAt(i) & 0x3ff))
-                utf8.push(0xf0 | (charcode >>18),
-                    0x80 | ((charcode>>12) & 0x3f),
-                    0x80 | ((charcode>>6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
-            }
-        }
+	/**
+	* Convert string to proper UTF-8 array
+	* @param str Input string
+	* @returns {Uint8Array} UTF8 array is returned as uint8 array
+	*/
+	function toUTF8Array (str) {
+		var utf8 = []
+		for (var i=0, n=str.length; i < n; i++) {
+			var charcode = str.charCodeAt(i)
+			if (charcode < 0x80) utf8.push(charcode)
+			else if (charcode < 0x800) {
+				utf8.push(0xc0 | (charcode >> 6),
+				0x80 | (charcode & 0x3f))
+			}
+			else if (charcode < 0xd800 || charcode >= 0xe000) {
+				utf8.push(0xe0 | (charcode >> 12),
+				0x80 | ((charcode>>6) & 0x3f),
+				0x80 | (charcode & 0x3f))
+			}
+			// surrogate pair
+			else {
+				i++;
+				// UTF-16 encodes 0x10000-0x10FFFF by
+				// subtracting 0x10000 and splitting the
+				// 20 bits of 0x0-0xFFFFF into two halves
+				charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+				| (str.charCodeAt(i) & 0x3ff))
+				utf8.push(0xf0 | (charcode >>18),
+				0x80 | ((charcode>>12) & 0x3f),
+				0x80 | ((charcode>>6) & 0x3f),
+				0x80 | (charcode & 0x3f))
+			}
+		}
 
-        return new Uint8Array(utf8);
-    }
+		return new Uint8Array(utf8)
+	}
 
 	/**
 	 * XXH object used as a constructor or a function
@@ -148,11 +148,12 @@
 		var isString = typeof input == 'string'
 		var isArrayBuffer
 
-        if(isString) {
-            input = toUTF8Array(input);
-            isString = typeof input == 'string'
-            isArrayBuffer = true
-        }
+		// Convert all strings to utf-8 first (issue #5)
+		if (isString) {
+			input = toUTF8Array(input)
+			isString = false
+			isArrayBuffer = true
+		}
 
 		if (input instanceof ArrayBuffer)
 		{
